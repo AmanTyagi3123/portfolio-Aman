68b95bbfcca9a36911263725e28d5b7a3f935645
Aman Tyagi <asang12a@gmail.com>
2025-02-23 15:24:16 +0530

    Making the hero section responsive.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,55 +1,52 @@
-import React, { useState } from 'react'
-import './hero.css'
-import ProjectData from '../data/ProjectData'
+import React, { useState } from 'react';
+import './hero.css';
+import ProjectData from '../data/ProjectData';
 import HeroSwiper from './HeroSwiper';
 import SocialLinksItem from './SocialLinksItem';
 
 function Hero() {
+  const [items, setItems] = useState(ProjectData);
+  const [activeItem, setActiveItem] = useState(items[0]); 
 
-  const [items, setItems] = useState(ProjectData)
-
-  const handleSlideChange= id=>{
-    const newItems = items.map(item=>{
-        item.active = false;
-        if(item._id===id){
-            item.active = true;
-        }
-        return item;
-    });
-    setItems(newItems);
+  const handleSlideChange = (id) => {
+    const newItem = items.find((item) => item._id === id);
+    if (newItem) {
+      setActiveItem(newItem);
+    }
   };
 
   return (
-    <> 
-            <div className="banner">
-            {
-              
-                items && items.length >0 && items.map(item=>(
-                    <div className="item" key={item._id}>
-                    <img src={item.bgImg} alt="" className={`bgImg ${item.active ? "active": undefined}`} />
-                    <div className={`content ${item.active ? "active": undefined}`}>    
-                        <p>
-                            {item.subtitle}
-                        </p>
-                        <h1>
-                            {item.title}
-                        </h1>
-                        <span className="social-links">
-                        <SocialLinksItem name='github' link='https://github.com/AmanTyagi3123'/>
-                        </span>
-                    </div>
-                    <div className="subtitle mb-10"><span className="slogan">The Project</span></div>
-                    <div className="subtitle">
-                        <span className={`number ${item.active ? "active": undefined}`}>#{item._id}</span>
-                    </div>
-                </div>
-                ))
-            }
-
-            {items && items.length>0 && (<HeroSwiper slides={items} slideChange={handleSlideChange} />)}
+    <>
+      <div className="banner">
+        <div className="item" key={activeItem._id}>
+          <img
+            src={activeItem.bgImg}
+            alt=""
+            className="bgImg active"
+          />
+          <div className="content active">
+            <p>{activeItem.subtitle}</p>
+            <h1>{activeItem.title}</h1>
+            <span className="social-links">
+              <SocialLinksItem
+                name="github"
+                link="https://github.com/AmanTyagi3123"
+              />
+            </span>
+          </div>
+          <div className="subtitle mb-10">
+            <span className="slogan">The Project</span>
+          </div>
+          <div className="subtitle">
+            <span className="number active">#{activeItem._id}</span>
+          </div>
         </div>
-        </>
-  )
+        {items && items.length > 0 && (
+          <HeroSwiper slides={items} slideChange={handleSlideChange} />
+        )}
+      </div>
+    </>
+  );
 }
 
-export default Hero
+export default Hero;
